58dd165c20f914146bece18e5c2da2e09bb92d39
Sam Kohn <kohn@berkeley.edu>
2019-04-08 16:48:16 -0700

    Add a bunch of comments

--- process.py
+++ process.py
@@ -19,6 +19,15 @@ from translate import (TreeBuffer, float_value, assign_value,
         fetch_value, int_value, unsigned_int_value, long_value)
 
 def done_with_cache(buf):
+    '''
+    Test whether an event should be removed from the event cache, based
+    on whether certain values have been assigned to it.
+
+    '''
+    # An event is done if it is a flasher, or if not, then if the next
+    # WSMuon and the next delayed-like events have been processed. (If
+    # the event is not an AD event then the delayed-like event does not
+    # have to have been processed.)
     flasher_done = buf.tag_flasher[0] != 0
     found_next_WSMuon = buf.dt_next_WSMuon[0] != 0
     detector = buf.noTree_detector[0]
@@ -35,6 +44,7 @@ def main(debug):
     indata = infile.Get('data')
     outdata = TTree('computed', 'Computed quantities by Sam Kohn')
 
+    # Initialize the "buffer" used to fill values into the TTree
     fill_buf = TreeBuffer()
     fill_buf.noTree_timestamp = long_value()
     fill_buf.noTree_detector = int_value()
@@ -61,6 +71,8 @@ def main(debug):
     fill_buf.num_PromptLikes_400us = unsigned_int_value()
     fill_buf.dts_PromptLikes_400us = long_value(200)
 
+    # Initialize the new TTree so that each TBranch reads from the
+    # appropriate TreeBuffer attribute
     outdata.Branch('fID', fill_buf.fID, 'fID/F')
     outdata.Branch('fPSD', fill_buf.fPSD, 'fPSD/F')
     outdata.Branch('tag_flasher', fill_buf.tag_flasher, 'tag_flasher/i')
@@ -100,6 +112,10 @@ def main(debug):
             fill_buf.dts_PromptLikes_400us,
             'dts_PromptLikes_400us[num_PromptLikes_400us]/L')
 
+    # Initialize the machinery for dt_next_* and dt_previous_*
+    # attributes. The event_cache stores the TreeBuffer for each event
+    # until the next WSMuon and next DelayedLike have been found, thus
+    # preserving the event order between the input and output TTrees.
     event_cache = deque()
     last_WSMuon_time = 0
     last_ADMuon_time = {n:0 for n in range(9)}
@@ -113,10 +129,12 @@ def main(debug):
     entries = xrange(indata.GetEntries()) if not debug else range(10000)
     for event_number in entries:
         logging.debug('event cache size: %d', len(event_cache))
+        # Load the current event in the input TTree
         indata.LoadTree(event_number)
         indata.GetEntry(event_number)
         buf = fill_buf.clone_type()
 
+        # Fetch the necessary values from the input TTree
         timestamp = fetch_value(indata, 'timeStamp', int)
         assign_value(buf.noTree_timestamp, timestamp)
         detector = fetch_value(indata, 'detector', int)
@@ -132,6 +150,8 @@ def main(debug):
         f2inch_maxQ = fetch_value(indata, 'f2inch_maxQ', float)
         energy = fetch_value(indata, 'energy', float)
 
+        # Compute simple tags and values (those that only require data
+        # from the current event)
         event_fID = fID(fMax, fQuad)
         assign_value(buf.fID, event_fID)
         event_fPSD = fPSD(fPSD_t1, fPSD_t2)
@@ -139,6 +159,8 @@ def main(debug):
         event_isFlasher = isFlasher(event_fID, event_fPSD, f2inch_maxQ)
         assign_value(buf.tag_flasher, event_isFlasher)
         if event_isFlasher:
+            # Flashers cannot be muons but they can be prompt-like or
+            # delayed-like
             event_isWSMuon = False
             event_isADMuon = False
             event_isShowerMuon = False
@@ -155,21 +177,29 @@ def main(debug):
         assign_value(buf.tag_DelayedLike, event_isDelayedLike)
 
 
-        # Remove muons that happened greater than MUON_COUNT_TIME ago
+        # Compute tags and values that count the number of previous
+        # events within a given time satisfying some criteria.
+
+        # Ensure the list of previous shower muons only has events more
+        # recent than MUON_COUNT_TIME ago
         while len(recent_shower_muons[detector]) > 0 and (timestamp
                 - recent_shower_muons[detector][0] > MUON_COUNT_TIME):
             recent_shower_muons[detector].popleft()
 
-        # Remove prompt-likes that happened greater than PROMPT_COUNT_TIME ago
+        # Ensure the list of previous prompt-like events only has events
+        # more recent than PROMPT_COUNT_TIME ago
         while len(recent_promptlikes[detector]) > 0 and (timestamp -
                 recent_promptlikes[detector][0] > PROMPT_COUNT_TIME):
             recent_promptlikes[detector].popleft()
 
+        # Compute the dts for both recent muons and recent prompt-likes
         recent_showermuon_dts = [timestamp - muon_time for muon_time in
                 recent_shower_muons[detector]]
         recent_promptlike_dts = [timestamp - prompt_time for prompt_time
                 in recent_promptlikes[detector]]
 
+        # Assign the values for dt_previous_* and for the lists of
+        # recent muons and prompt-likes.
         assign_value(buf.dt_previous_WSMuon, timestamp -
                 last_WSMuon_time)
         assign_value(buf.dt_previous_ADMuon, timestamp -
@@ -187,22 +217,26 @@ def main(debug):
         for i, dt in enumerate(recent_promptlike_dts):
             assign_value(buf.dts_PromptLikes_400us, dt, i)
 
-        # Compute muon vetoes
+        # Compute muon vetoes that only rely on previous events
         assign_value(buf.tag_ADMuonVeto,
                 muons.isVetoedByADMuon(buf.dt_previous_ADMuon[0]))
         assign_value(buf.tag_ShowerMuonVeto,
                 muons.isVetoedByShowerMuon(buf.dt_previous_ShowerMuon[0]))
 
         # Update the dt_previous_* and dt_next_* values
-        #
-        # Ths comes after values are assigned to the buffer because we
+
+        # This comes after values are assigned to the buffer because we
         # don't want dt_previous_* to be 0.
         if event_isWSMuon:
             logging.debug("isWSMuon")
             last_WSMuon_time = timestamp
+            # Assign dt_next_WSMuon to the events in the event_cache
             for cached_event in event_cache:
+                # Some events might already have been assigned, so skip
+                # those
                 if cached_event.dt_next_WSMuon[0] != 0:
                     continue
+                # WSMuons are common across the entire EH/site
                 if cached_event.noTree_site[0] == site:
                     logging.debug('taggedNextWS%d', cached_event.noTree_timestamp[0])
                     assign_value(cached_event.dt_next_WSMuon,
@@ -215,14 +249,20 @@ def main(debug):
         if event_isShowerMuon:
             last_ShowerMuon_time[detector] = timestamp
             recent_shower_muons[detector].append(timestamp)
+        # Don't include prompt-like flasher events
         if event_isPromptLike and not event_isFlasher:
             last_PromptLike_time[detector] = timestamp
             recent_promptlikes[detector].append(timestamp)
+        # Don't include prompt-like delayed events
         if event_isDelayedLike and not event_isFlasher:
             logging.debug("isDelayedLike")
+            # Assign dt_next_PromptLike to events in the event_cache
             for cached_event in event_cache:
+                # Some events might already have been assigned, so skip
+                # those
                 if cached_event.dt_next_DelayedLike[0] != 0:
                     continue
+                # PromptLikes are restricted to a single AD
                 if cached_event.noTree_detector[0] == detector:
                     logging.debug('taggedNextDelayed%d',
                             cached_event.noTree_timestamp[0])
@@ -258,13 +298,14 @@ def main(debug):
         if debug:
             pass #time.sleep(0.1)
         #outdata.Fill()
+    # After the event loop is finished, fill the remaining events from
+    # the event_cache into the output TTree
     for cached_event in event_cache:
         assign_value(cached_event.dt_next_WSMuon, -1)
         assign_value(cached_event.tag_WSMuonVeto, 2)
         assign_value(cached_event.dt_next_DelayedLike, -1)
         cached_event.copyTo(fill_buf)
         outdata.Fill()
-    event_cache = []
 
     infile.Write()
     infile.Close()

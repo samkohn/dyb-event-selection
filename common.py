2b966dd7e807e2608172cf2eef1d0d84b3bc51b3
Sam Kohn <kohn@berkeley.edu>
2019-04-08 14:51:08 -0700

    Acutally add common.py

---
+++ common.py
@@ -0,0 +1,8 @@
+__all__ = [
+        "ALL_DETECTORS",
+        "AD_DETECTORS",
+        "WP_DETECTORS",
+        ]
+ALL_DETECTORS = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+AD_DETECTORS = [1, 2, 3, 4]
+WP_DETECTORS = [5, 6]

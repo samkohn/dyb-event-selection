b1bcb2ea2b7761e1d7d1881d12bddbc83f40b7e1
Sam Kohn <kohn@berkeley.edu>
2019-04-30 18:11:29 -0700

    Refactor rate_calculations for use in fullstack

--- rate_calculations.py
+++ rate_calculations.py
@@ -11,20 +11,24 @@ from ROOT import TFile, TTree
 from translate import fetch_value
 import muons
 
+class RateHelper(object):
+    def __init__(self):
+        self.total_nonvetoed_livetime = {1:0, 2:0}  # by AD
+        self.start_time = 0
+        self.end_time = 0
+        self.next_livetime_start = {1:0, 2:0}
+        self.number_IBD_candidates = {1:0, 2:0}
+        self.number_prompts = {1:0, 2:0}
+        self.number_delayeds = {1:0, 2:0}
+        self.events_since_last_valid_time = 0
+
 def main(num_events, start_event, debug):
     filename = 'out.root'
     infile = TFile(filename, 'READ')
     indata = infile.Get('data')
     incomputed = infile.Get('computed')
 
-    total_nonvetoed_livetime = {1:0, 2:0}  # by AD
-    start_time = 0
-    end_time = 0
-    next_livetime_start = {1:0, 2:0}
-    number_IBD_candidates = {1:0, 2:0}
-    number_prompts = {1:0, 2:0}
-    number_delayeds = {1:0, 2:0}
-    events_since_last_valid_time = 0
+    helper = RateHelper()
 
     total_entries = min(indata.GetEntries(), incomputed.GetEntries())
     entries = min(num_events+start_event, total_entries) if num_events > 0 else total_entries
@@ -34,25 +38,80 @@ def main(num_events, start_event, debug):
         incomputed.LoadTree(event_number)
         incomputed.GetEntry(event_number)
         logging.debug('Event %d', event_number)
+        data_list = fetch_data(indata, incomputed)
+        one_iteration(event_number, data_list, helper, start_event, entries)
+    print_results(helper)
 
-        timestamp = fetch_value(indata, 'timeStamp', int)
-        detector = fetch_value(indata, 'detector', int)
-        isWSMuon = fetch_value(incomputed, 'tag_WSMuon', bool)
-        isADMuon = fetch_value(incomputed, 'tag_ADMuon', bool)
-        isShowerMuon = fetch_value(incomputed, 'tag_ShowerMuon', bool)
-        isIBDDelayed = fetch_value(incomputed, 'tag_IBDDelayed', bool)
-        isPromptLike = fetch_value(incomputed, 'tag_PromptLike', bool)
-        isDelayedLike = fetch_value(incomputed, 'tag_DelayedLike', bool)
-        isWSMuonVetoed = fetch_value(incomputed, 'tag_WSMuonVeto', bool)
-        isADMuonVetoed = fetch_value(incomputed, 'tag_ADMuonVeto', bool)
-        isShowerMuonVetoed = fetch_value(incomputed,
-                'tag_ShowerMuonVeto', bool)
-        isFlasher = fetch_value(incomputed, 'tag_flasher', bool)
-        dt_last_WSMuon = fetch_value(incomputed, 'dt_previous_WSMuon', int)
-        dt_next_WSMuon = fetch_value(incomputed, 'dt_next_WSMuon', int)
-        dt_last_ADMuon = fetch_value(incomputed, 'dt_previous_ADMuon', int)
-        dt_last_ShowerMuon = fetch_value(incomputed,
-                'dt_previous_ShowerMuon', int)
+def print_results(helper):
+    print('total DAQ livetime:')
+    daq_livetime = helper.end_time - helper.start_time
+    print(daq_livetime)
+    print('total nonvetoed livetime:')
+    print(helper.total_nonvetoed_livetime)
+    efficiency = {n: float(nonvetoed)/daq_livetime for n, nonvetoed in
+            helper.total_nonvetoed_livetime.items()}
+    print('efficiency:')
+    print(efficiency)
+    nonvetoed_livetime_days = {n: t/(1e9 * 60 * 60 * 24) for n, t in
+            helper.total_nonvetoed_livetime.items()}
+    print('IBD candidates:')
+    print(helper.number_IBD_candidates)
+    print('delayed-like:')
+    print(helper.number_delayeds)
+    print('prompt-like:')
+    print(helper.number_prompts)
+    print('IBD rate per day:')
+    print({n: num/nonvetoed_livetime_days[n] for n, num in
+        helper.number_IBD_candidates.items()})
+    print('delayed-like rate (Hz):')
+    print({n: float(num)/helper.total_nonvetoed_livetime[n]*1e9 for n, num in
+        helper.number_delayeds.items()})
+    print('prompt-like rate (Hz):')
+    print({n: float(num)/helper.total_nonvetoed_livetime[n]*1e9 for n, num in
+        helper.number_prompts.items()})
+
+def callback_adapter(helper, start_event, entries):
+    def callback(event_cache):
+        data_list = []
+        data_list.append(event_cache.noTree_timestamp[0])
+        data_list.append(event_cache.noTree_detector[0])
+        data_list.append(event_cache.tag_WSMuon[0])
+        data_list.append(event_cache.tag_ADMuon[0])
+        data_list.append(event_cache.tag_ShowerMuon[0])
+        data_list.append(event_cache.tag_IBDDelayed[0])
+        data_list.append(event_cache.tag_PromptLike[0])
+        data_list.append(event_cache.tag_DelayedLike[0])
+        data_list.append(event_cache.tag_WSMuonVeto[0])
+        data_list.append(event_cache.tag_ADMuonVeto[0])
+        data_list.append(event_cache.tag_ShowerMuonVeto[0])
+        data_list.append(event_cache.tag_flasher[0])
+        data_list.append(event_cache.dt_previous_WSMuon[0])
+        data_list.append(event_cache.dt_next_WSMuon[0])
+        data_list.append(event_cache.dt_previous_ADMuon[0])
+        data_list.append(event_cache.dt_previous_ShowerMuon[0])
+        event_number = event_cache.noTree_loopIndex[0]
+        one_iteration(event_number, data_list, helper, start_event, entries)
+    return callback
+
+def one_iteration(event_number, data_list, helper, start_event, entries):
+        (
+            timestamp,
+            detector,
+            isWSMuon,
+            isADMuon,
+            isShowerMuon,
+            isIBDDelayed,
+            isPromptLike,
+            isDelayedLike,
+            isWSMuonVetoed,
+            isADMuonVetoed,
+            isShowerMuonVetoed,
+            isFlasher,
+            dt_last_WSMuon,
+            dt_next_WSMuon,
+            dt_last_ADMuon,
+            dt_last_ShowerMuon
+            ) = data_list
 
         muon_type = 'None'
         if isWSMuon:
@@ -65,21 +124,21 @@ def main(num_events, start_event, debug):
 
 
         if event_number == start_event:
-            start_time = timestamp
-            for key in next_livetime_start:
-                next_livetime_start[key] = (timestamp +
+            helper.start_time = timestamp
+            for key in helper.next_livetime_start:
+                helper.next_livetime_start[key] = (timestamp +
                         muons._SHOWER_MUON_VETO_LAST_NS)
         if event_number == entries - 1:
-            end_time = timestamp
+            helper.end_time = timestamp
 
         if isIBDDelayed:
-            number_IBD_candidates[detector] += 1
+            helper.number_IBD_candidates[detector] += 1
         if (isDelayedLike
                 and not isFlasher
                 and not isWSMuonVetoed
                 and not isADMuonVetoed
                 and not isShowerMuonVetoed):
-            number_delayeds[detector] += 1
+            helper.number_delayeds[detector] += 1
         EXTRA_PROMPT_DT = 400e3
         if (isPromptLike
                 and not isFlasher
@@ -91,63 +150,75 @@ def main(num_events, start_event, debug):
                 and dt_last_ShowerMuon >
                     (muons._SHOWER_MUON_VETO_LAST_NS
                         - EXTRA_PROMPT_DT)):
-            number_prompts[detector] += 1
+            helper.number_prompts[detector] += 1
 
-        logging.debug('next livetime start: %s', next_livetime_start)
+        logging.debug('next livetime start: %s', helper.next_livetime_start)
         logging.debug('timestamp: %d', timestamp)
         if isWSMuon:
-            for det, next_start in next_livetime_start.items():
+            for det, next_start in helper.next_livetime_start.items():
                 new_livetime = (timestamp - muons._WSMUON_VETO_NEXT_NS
                         - next_start)
                 if new_livetime > 0:
-                    total_nonvetoed_livetime[det] += new_livetime
+                    helper.total_nonvetoed_livetime[det] += new_livetime
                 new_start = timestamp + muons._WSMUON_VETO_LAST_NS
                 if new_start > next_start:
-                    next_livetime_start[det] = new_start
+                    helper.next_livetime_start[det] = new_start
         if isADMuon:
-            next_start = next_livetime_start[detector]
+            next_start = helper.next_livetime_start[detector]
             new_livetime = timestamp - next_start
             if new_livetime > 0:
-                total_nonvetoed_livetime[detector] += new_livetime
+                helper.total_nonvetoed_livetime[detector] += new_livetime
             new_start = timestamp + muons._ADMUON_VETO_LAST_NS
             if new_start > next_start:
-                next_livetime_start[detector] = new_start
+                helper.next_livetime_start[detector] = new_start
         if isShowerMuon:
-            next_start = next_livetime_start[detector]
+            next_start = helper.next_livetime_start[detector]
             new_livetime = timestamp - next_start
             if new_livetime > 0:
-                total_nonvetoed_livetime[detector] += new_livetime
+                helper.total_nonvetoed_livetime[detector] += new_livetime
             new_start = timestamp + muons._SHOWER_MUON_VETO_LAST_NS
             if new_start > next_start:
-                next_livetime_start[detector] = new_start
-        logging.debug('new next livetime start: %s', next_livetime_start)
-        logging.debug('new total: %s', total_nonvetoed_livetime)
-    print('total DAQ livetime:')
-    daq_livetime = end_time - start_time
-    print(daq_livetime)
-    print('total nonvetoed livetime:')
-    print(total_nonvetoed_livetime)
-    efficiency = {n: float(nonvetoed)/daq_livetime for n, nonvetoed in
-            total_nonvetoed_livetime.items()}
-    print('efficiency:')
-    print(efficiency)
-    nonvetoed_livetime_days = {n: t/(1e9 * 60 * 60 * 24) for n, t in
-            total_nonvetoed_livetime.items()}
-    print('IBD candidates:')
-    print(number_IBD_candidates)
-    print('delayed-like:')
-    print(number_delayeds)
-    print('prompt-like:')
-    print(number_prompts)
-    print('IBD rate per day:')
-    print({n: num/nonvetoed_livetime_days[n] for n, num in
-        number_IBD_candidates.items()})
-    print('delayed-like rate (Hz):')
-    print({n: float(num)/total_nonvetoed_livetime[n]*1e9 for n, num in
-        number_delayeds.items()})
-    print('prompt-like rate (Hz):')
-    print({n: float(num)/total_nonvetoed_livetime[n]*1e9 for n, num in
-        number_prompts.items()})
+                helper.next_livetime_start[detector] = new_start
+        logging.debug('new next livetime start: %s', helper.next_livetime_start)
+        logging.debug('new total: %s', helper.total_nonvetoed_livetime)
+
+def fetch_data(indata, incomputed):
+    timestamp = fetch_value(indata, 'timeStamp', int)
+    detector = fetch_value(indata, 'detector', int)
+    isWSMuon = fetch_value(incomputed, 'tag_WSMuon', bool)
+    isADMuon = fetch_value(incomputed, 'tag_ADMuon', bool)
+    isShowerMuon = fetch_value(incomputed, 'tag_ShowerMuon', bool)
+    isIBDDelayed = fetch_value(incomputed, 'tag_IBDDelayed', bool)
+    isPromptLike = fetch_value(incomputed, 'tag_PromptLike', bool)
+    isDelayedLike = fetch_value(incomputed, 'tag_DelayedLike', bool)
+    isWSMuonVetoed = fetch_value(incomputed, 'tag_WSMuonVeto', bool)
+    isADMuonVetoed = fetch_value(incomputed, 'tag_ADMuonVeto', bool)
+    isShowerMuonVetoed = fetch_value(incomputed,
+            'tag_ShowerMuonVeto', bool)
+    isFlasher = fetch_value(incomputed, 'tag_flasher', bool)
+    dt_last_WSMuon = fetch_value(incomputed, 'dt_previous_WSMuon', int)
+    dt_next_WSMuon = fetch_value(incomputed, 'dt_next_WSMuon', int)
+    dt_last_ADMuon = fetch_value(incomputed, 'dt_previous_ADMuon', int)
+    dt_last_ShowerMuon = fetch_value(incomputed,
+            'dt_previous_ShowerMuon', int)
+    return (
+            timestamp,
+            detector,
+            isWSMuon,
+            isADMuon,
+            isShowerMuon,
+            isIBDDelayed,
+            isPromptLike,
+            isDelayedLike,
+            isWSMuonVetoed,
+            isADMuonVetoed,
+            isShowerMuonVetoed,
+            isFlasher,
+            dt_last_WSMuon,
+            dt_next_WSMuon,
+            dt_last_ADMuon,
+            dt_last_ShowerMuon
+            )
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()

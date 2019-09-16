a40017e08b1bde8f136043c7641aa91d86eec6ad
Sam Kohn <kohn@berkeley.edu>
2019-09-16 15:37:15 -0700

    Add US nH analysis muon selection and vetoes

--- muons.py
+++ muons.py
@@ -37,3 +37,35 @@ def isVetoedByADMuon(dt_last_ad):
 def isVetoedByShowerMuon(dt_last_shower):
     return (dt_last_shower < _SHOWER_MUON_VETO_LAST_NS)
 
+_NH_IWS_WSMUON_NHIT_THRESHOLD = 12
+_NH_OWS_WSMUON_NHIT_THRESHOLD = 15
+_NH_ADMUON_THRESHOLD = 20
+_NH_SHOWER_MUON_THRESHOLD = 2500
+
+_NH_WSMUON_VETO_LAST_NS = int(400e3)
+_NH_ADMUON_VETO_LAST_NS = int(800e3)
+_NH_SHOWER_MUON_VETO_LAST_NS = int(1e9)
+
+def isWSMuon_nH(event_detector, event_nHit, event_triggerType):
+    return (event_detector in WP_DETECTORS and
+            event_nHit > (_NH_IWS_WSMUON_NHIT_THRESHOLD if event_detector == 5 else
+                _NH_OWS_WSMUON_NHIT_THRESHOLD) and
+            (event_triggerType & 0x2) == 0)
+
+def isADMuon_nH(event_detector, event_energy):
+    return (event_detector in AD_DETECTORS
+            and event_energy > _NH_ADMUON_THRESHOLD
+            and event_energy <= _NH_SHOWER_MUON_THRESHOLD)
+
+def isShowerMuon_nH(event_detector, event_energy):
+    return (event_detector in AD_DETECTORS
+            and event_energy > _NH_SHOWER_MUON_THRESHOLD)
+
+def isVetoedByWSMuon_nH(dt_last_ws):
+    return (dt_last_ws < _NH_WSMUON_VETO_LAST_NS)
+
+def isVetoedByADMuon_nH(dt_last_ad):
+    return (dt_last_ad < _NH_ADMUON_VETO_LAST)
+
+def isVetoedByShowerMuon_nH(dt_last_shower):
+    return (dt_last_shower < _NH_SHOWER_MUON_VETO_LAST_NS)

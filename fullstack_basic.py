eacd2387ad5922fe645efb52c7a78020c60c225c
Sam Kohn <kohn@berkeley.edu>
2019-10-25 13:41:21 -0700

    Add IBD selection to fullstack_basic.py

--- fullstack_basic.py
+++ fullstack_basic.py
@@ -8,32 +8,38 @@ import logging
 import json
 import re
 import subprocess
+import os.path
 
 def extract_run_fileno(filename):
     run = int(re.search('\d{7}', filename).group(0))
     fileno = int(re.search('_\d{4}', filename).group(0)[1:])
     return (run, fileno)
 
-def main(filename, nevents, start_event, site):
+def main(filename, outdir, nevents, start_event, site, selection_name):
     from ROOT import TTree, TFile, TChain
 
     import translate
-    import process_basic as process
+    import process_basic
+    import process
     if filename is None:
         filename = "/project/projectdirs/dayabay/data/exp/dayabay/2015/p15a/Neutrino/0126/recon.Neutrino.0050958.Physics.EH1-Merged.P15A-P._0001.root"
         site = 1
 
     run, fileno = extract_run_fileno(filename)
 
-    outfile = TFile('basic_%d_%04d.root' % (run, fileno), 'RECREATE',
+    # Stage 1: create slimmed-down "basic" files
+    outfilepath = os.path.join(outdir, 'basic', 'basic_%d_%04d.root'
+            % (run, fileno))
+
+    outfile = TFile(outfilepath, 'RECREATE',
             'DYB Run %d file %d, git %s' % (run, fileno,
                 translate.git_describe()))
     #outdata, outdata_buf = translate.create_data_TTree(outfile)
-    computed, computed_buf = process.create_computed_TTree('slimmed', outfile,
+    computed, computed_buf = process_basic.create_computed_TTree('slimmed', outfile,
             'Slimmed down dataset with muons, high-energy events and '
             'coincidences (git: %s)')
     calibStats, adSimple = translate.initialize_indata([filename])
-    computed_helper = process.ProcessHelper()
+    computed_helper = process_basic.ProcessHelper()
     computed_helper.run = run
     computed_helper.fileno = fileno
     end_event = (calibStats.GetEntries() if nevents == -1 else
@@ -51,28 +57,39 @@ def main(filename, nevents, start_event, site):
 
         translate.copy(computed_buf, calibStats, adSimple, run, fileno)
 
-        process.one_iteration(entry_number, computed,
+        process_basic.one_iteration(entry_number, computed,
                 computed_buf, computed_helper)
     # After the event loop is finished, fill the remaining events from
     # the event_cache into the output TTree
-    process.finish_emptying_cache(computed, computed_buf,
+    process_basic.finish_emptying_cache(computed, computed_buf,
             computed_helper.event_cache)
     outfile.Write()
     outfile.Close()
 
+    # Stage 2: perform IBD selection
+    infilepath = outfilepath  # old outfile path
+    outfilepath = os.path.join(outdir, 'out_%s' % selection_name, 'out_%s_%d_%04d.root'
+            % (selection_name, run, fileno))
+
+    process.main(-1, infilepath, outfilepath, selection_name, False)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--infile', default=None)
+    parser.add_argument('-o', '--outdir', default='./')
     parser.add_argument('-n', '--num-events', default=-1, type=int)
     parser.add_argument('-s', '--start-event', default=0, type=int)
     parser.add_argument('-d', '--debug', action='store_true')
+    parser.add_argument('--selection')
     parser.add_argument('--site', type=int)
     parser.add_argument('--lineno', type=int, default=0)
     args = parser.parse_args()
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
     try:
-        main(args.infile, args.num_events, args.start_event, args.site)
+        outdir = os.path.abspath(os.path.expanduser(args.outdir))
+        main(args.infile, outdir, args.num_events, args.start_event, args.site,
+                args.selection)
     except Exception as e:
         logging.exception(e)
         subprocess.check_output(['touch',

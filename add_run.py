8d4421364c30435e53e11986e5216b2c2184fe5e
Sam Kohn <kohn@berkeley.edu>
2019-12-18 13:22:49 -0800

    Add script for registering a data run into the database

---
+++ add_run.py
@@ -0,0 +1,18 @@
+import argparse
+import json
+import sqlite3
+
+parser = argparse.ArgumentParser()
+parser.add_argument('infile')
+args = parser.parse_args()
+
+with open(args.infile, 'r') as f:
+    data = json.load(f)
+run = data['run']
+hall = data['site']
+start_time = int(data['start_time']/1e9)
+
+with sqlite3.Connection('parameters.db') as conn:
+    c = conn.cursor()
+    c.execute('INSERT OR REPLACE INTO runs VALUES (?, ?, ?)', (run, hall,
+        start_time))

f6eb7d7e6eb18f781c4573b792361c86cd246a75
Sam Kohn <kohn@berkeley.edu>
2020-02-07 14:26:11 -0800

    Update job-producer.py to be much more flexible
    It now takes a command template that is passed to python's string.format so you
    can use any command and plug in the data file location and run and file
    numbers.

--- job-producer.py
+++ job-producer.py
@@ -4,8 +4,9 @@ import os.path
 import subprocess
 
 def get_run_info(line_number, filename):
-    line_output = subprocess.check_output(['sed', '%dq;d' % line_number,
-        filename])
+    command_result = subprocess.run(['sed', '%dq;d' % line_number,
+        filename], capture_output=True)
+    line_output = command_result.stdout.decode()
     result = re.match('^(\d+)\s+(\d+)\s+(\d).*$', line_output)
     run = int(result.group(1))
     fileno = int(result.group(2))
@@ -14,50 +15,40 @@ def get_run_info(line_number, filename):
 
 def get_file_location(run, fileno):
     find_file=os.path.expanduser('~mkramer/projscratch/p17b/code/p17b_find/p17b_find')
-    location = subprocess.check_output([find_file, str(run), str(fileno)])
+    command_result = subprocess.run([find_file, str(run), str(fileno)],
+            capture_output=True)
+    location = command_result.stdout.decode()
     return location.strip()
 
-def generate_worker_command(line, runlist, selection_name, prefix='python '):
+def generate_worker_command(line, runlist, command_template):
     run, fileno, site = get_run_info(line, runlist)
     infile = get_file_location(run, fileno)
-    command = prefix + ('{script} -i {filepath} -n {nevents} --site {site} '
-    '--selection {selection}').format(
-            filepath=infile, nevents=-1, site=site, script=jobscript,
-            selection=selection_name)
+    command = command_template.format(filepath=infile, run=run, fileno=fileno,
+            site=site)
     return command
 
-def generate_worker_command_basic(line, runlist, selection_name, outdir,
-        prefix='python '):
-    run, fileno, site = get_run_info(line, runlist)
-    infile = get_file_location(run, fileno)
-    command = prefix + ('{script} -i {filepath} -o {outdir}'
-        ' --selection {name}').format(script=jobscript, filepath=infile,
-                nevents=-1, outdir=outdir, name=selection_name)
-    return command
+parser = argparse.ArgumentParser(epilog=
+        '''Command template variables:
+  - {filepath} = path to input file
+  - {run} = run number, as an int
+  - {fileno} = sub-run / file number, as an int
+  - {site} = site / hall number (1, 2, or 3)
 
-parser = argparse.ArgumentParser()
+The command template will be passed to python's string.format function
+so the template can contain more detailed formatting instructions (such
+as zero-padding the fileno using {fileno:>04}).''',
+formatter_class=argparse.RawDescriptionHelpFormatter)
+
 parser.add_argument('runlist')
 parser.add_argument('ntasks', type=int)
 parser.add_argument('--start-task', type=int, default=1)
-parser.add_argument('--jobscript')
-parser.add_argument('--selection')
-parser.add_argument('--basic', action='store_true')
-parser.add_argument('-o', '--output')
-parser.add_argument('--outdir', default='./')
+parser.add_argument('command', metavar='COMMAND_TEMPLATE')
 args = parser.parse_args()
 runlist = args.runlist
 ntasks = args.ntasks
 start_task = args.start_task
-jobscript = args.jobscript
-outfile = args.output
-selection = args.selection
+command_template = args.command
 lines_to_read = range(start_task, ntasks+start_task)
-with open(outfile, 'w') as f:
-    for line in lines_to_read:
-        if args.basic:
-            command = generate_worker_command_basic(line, runlist, selection,
-                    args.outdir, prefix='job.sh ')
-        else:
-            command = generate_worker_command(line, runlist, selection,
-                    prefix='job.sh ')
-        f.write(command + '\n')
+for line in lines_to_read:
+    command = generate_worker_command(line, runlist, command_template)
+    print(command)

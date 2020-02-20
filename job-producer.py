17feb3dd555a924a323044211fed554082875acb
Sam Kohn <kohn@berkeley.edu>
2020-02-20 12:39:38 -0800

    Update job-producer to be faster and take more input formats
    Now takes both <run> <file> <site> <timestamp> (file list) and <run>
    <site> <timestamp> (run list)

--- job-producer.py
+++ job-producer.py
@@ -3,15 +3,18 @@ import re
 import os.path
 import subprocess
 
-def get_run_info(line_number, filename):
-    command_result = subprocess.run(['sed', '%dq;d' % line_number,
-        filename], capture_output=True)
-    line_output = command_result.stdout.decode()
-    result = re.match('^(\d+)\s+(\d+)\s+(\d).*$', line_output)
+def get_run_info(line):
+    result = re.match('^(\d+)\s+(\d+)\s+(\d).*$', line)
     run = int(result.group(1))
     fileno = int(result.group(2))
     site = int(result.group(3))
     return (run, fileno, site)
+
+def get_run_info_run_only(line):
+    result = re.match('^(\d+)\s+(\d+).*$', line)
+    run = int(result.group(1))
+    site = int(result.group(2))
+    return (run, site)
 
 def get_file_location(run, fileno):
     find_file=os.path.expanduser('~mkramer/projscratch/p17b/code/p17b_find/p17b_find')
@@ -20,11 +23,15 @@ def get_file_location(run, fileno):
     location = command_result.stdout.decode()
     return location.strip()
 
-def generate_worker_command(line, runlist, command_template):
-    run, fileno, site = get_run_info(line, runlist)
-    infile = get_file_location(run, fileno)
-    command = command_template.format(filepath=infile, run=run, fileno=fileno,
-            site=site)
+def generate_worker_command(line, command_template, run_only):
+    if run_only:
+        run, site = get_run_info_run_only(line)
+        args = {'run': run, 'site': site}
+    else:
+        run, fileno, site = get_run_info(line)
+        infile = get_file_location(run, fileno)
+        args = {'run': run, 'fileno': fileno, 'site': site, 'filepath': infile}
+    command = command_template.format(**args)
     return command
 
 parser = argparse.ArgumentParser(epilog=
@@ -43,12 +50,17 @@ parser.add_argument('runlist')
 parser.add_argument('ntasks', type=int)
 parser.add_argument('--start-task', type=int, default=1)
 parser.add_argument('command', metavar='COMMAND_TEMPLATE')
+parser.add_argument('--run-only', action='store_true')
 args = parser.parse_args()
 runlist = args.runlist
 ntasks = args.ntasks
 start_task = args.start_task
 command_template = args.command
-lines_to_read = range(start_task, ntasks+start_task)
-for line in lines_to_read:
-    command = generate_worker_command(line, runlist, command_template)
-    print(command)
+with open(runlist, 'r') as fin:
+    for i, line in enumerate(fin):
+        if i < start_task - 1:
+            continue
+        if i >= ntasks + start_task - 1:
+            break
+        command = generate_worker_command(line, command_template, args.run_only)
+        print(command)

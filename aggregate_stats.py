ad99ec74bc7dc0a31bb425e827322c99cb4d3c10
Sam Kohn <kohn@berkeley.edu>
2020-04-20 15:18:57 -0700

    Update aggregate_stats.py to save muon efficiency to the database

--- aggregate_stats.py
+++ aggregate_stats.py
@@ -19,6 +19,8 @@ def main2(run, files, site, ad, outfile, db):
         daq_livetime += results['daq_livetime']
         usable_livetime += results['usable_livetime']
         num_veto_windows += results['num_veto_windows']
+    efficiency = usable_livetime / daq_livetime
+    rate = num_veto_windows*1e9/usable_livetime
     with open(outfile, 'w') as f:
         json.dump({
             'run': run,
@@ -26,16 +28,16 @@ def main2(run, files, site, ad, outfile, db):
             'ad': ad,
             'daq_livetime': daq_livetime,
             'usable_livetime': usable_livetime,
-            'usable_fraction': usable_livetime/daq_livetime,
+            'usable_fraction': efficiency,
             'num_veto_windows': num_veto_windows,
             }, f)
     if db is not None:
         with sqlite3.Connection(db) as conn:
             cursor = conn.cursor()
             cursor.execute('INSERT OR REPLACE INTO muon_rates '
-                    'VALUES (?, ?, ?, ?, ?)',
+                    'VALUES (?, ?, ?, ?, ?, ?)',
                     (run, ad, num_veto_windows, usable_livetime,
-                        num_veto_windows*1e9/usable_livetime))
+                        rate, efficiency))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()

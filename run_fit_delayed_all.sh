704bc85f34d1d93c0c5dc61fc4a1d4fceda588f0
Sam Kohn <kohn@berkeley.edu>
2020-05-14 11:59:13 -0700

    Add script to run delayed energy fits on all ADs

---
+++ run_fit_delayed_all.sh
@@ -0,0 +1,8 @@
+python fit_delayed.py --site 1 --ad 1 $SCRATCH/dyb30/EH1/sub_ad1/sub_ad1.root -o fit_eh1_ad1.pdf --update-db $SCRATCH/dyb30/parameters.db
+python fit_delayed.py --site 1 --ad 2 $SCRATCH/dyb30/EH1/sub_ad2/sub_ad2.root -o fit_eh1_ad2.pdf --update-db $SCRATCH/dyb30/parameters.db
+python fit_delayed.py --site 2 --ad 1 $SCRATCH/dyb30/EH2/sub_ad1/sub_ad1.root -o fit_eh2_ad1.pdf --update-db $SCRATCH/dyb30/parameters.db
+python fit_delayed.py --site 2 --ad 2 $SCRATCH/dyb30/EH2/sub_ad2/sub_ad2.root -o fit_eh2_ad2.pdf --update-db $SCRATCH/dyb30/parameters.db
+python fit_delayed.py --site 3 --ad 1 $SCRATCH/dyb30/EH3/sub_ad1/sub_ad1.root -o fit_eh3_ad1.pdf --update-db $SCRATCH/dyb30/parameters.db
+python fit_delayed.py --site 3 --ad 2 $SCRATCH/dyb30/EH3/sub_ad2/sub_ad2.root -o fit_eh3_ad2.pdf --update-db $SCRATCH/dyb30/parameters.db
+python fit_delayed.py --site 3 --ad 3 $SCRATCH/dyb30/EH3/sub_ad3/sub_ad3.root -o fit_eh3_ad3.pdf --update-db $SCRATCH/dyb30/parameters.db
+python fit_delayed.py --site 3 --ad 4 $SCRATCH/dyb30/EH3/sub_ad4/sub_ad4.root -o fit_eh3_ad4.pdf --update-db $SCRATCH/dyb30/parameters.db

e3e3481c1f1a0066202a02078d246fe3f9dd7a6d
Sam Kohn <kohn@berkeley.edu>
2020-05-27 17:58:54 -0700

    Update compute_delayed_efficiency to use calorimeter function

--- compute_delayed_efficiency.py
+++ compute_delayed_efficiency.py
@@ -8,28 +8,34 @@ def main(infilename, outfilename, database):
     infile = ROOT.TFile(infilename, 'READ')
     data = infile.Get('toy')
     delayed_energy = 'res_d'
-    selection_all = 'target >= 0 && Volume_nCap >= 0'
-    delayed_spectrum = ROOT.TH1F('spectrum', 'spectrum', 400, 1, 5)
+    selection_all = 'target >= 0 && Volume_nCap >= 0 && res_p > 1.5'
+    delayed_spectrum = ROOT.TH1F('spectrum', 'spectrum', 525, 1.5, 6.75)
+    delayed_spectrum.Sumw2()
     data.Draw(delayed_energy + '>>spectrum', selection_all, 'goff')
     mu0 = 2.3
     sigma0 = 0.14
-    alpha0 = 1.5
-    n0 = 2
-    norm0 = delayed_spectrum.GetMaximum()
-    fitter = ROOT.TF1("cb_fitter", fit_delayed.crystal_ball, 1.8, 3.1, 5)
-    fitter.SetParameters(mu0, sigma0, alpha0, n0, norm0)
+    alpha0 = 0.8
+    scale0 = 2
+    approx_integral = delayed_spectrum.Integral(1,
+            delayed_spectrum.GetNbinsX())
+    bin_width = delayed_spectrum.GetBinWidth(1)
+    norm0 = approx_integral * bin_width
+    fitter = ROOT.TF1("calo_fitter", fit_delayed.calorimeter_fn, 1.8, 3.1, 5)
+    fitter.SetParameters(mu0, sigma0, scale0, alpha0, norm0)
     if outfilename is None:
         options = 'QN0S'
     else:
         options = 'QS'
     fit_result = delayed_spectrum.Fit(fitter, options)
-    mu, sigma, alpha, n, norm = [fit_result.Parameter(i) for i in range(5)]
+    mu, sigma, scale, alpha, norm = [fit_result.Parameter(i) for i in range(5)]
+    mu_err, sigma_err, scale_err, alpha_err, norm_err = [fit_result.ParError(i)
+            for i in range(5)]
     delayed_spectrum.GetXaxis().SetRangeUser(1.5, 3.4)
     if outfilename is not None:
         ROOT.gPad.Print(outfilename)
     if database is None:
-        print('mu, sigma, alpha, n, norm')
-        print(mu, sigma, alpha, n, norm)
+        print('mu, sigma, scale, alpha, norm')
+        print(mu, sigma, scale, alpha, norm)
     else:
         # Special values for site and ad for MC results
         site = 0
@@ -37,15 +43,16 @@ def main(infilename, outfilename, database):
         with sqlite3.Connection(database) as conn:
             c = conn.cursor()
             c.execute('''INSERT OR REPLACE INTO delayed_energy_fits
-            VALUES (?, ?, ?, ?, ?, ?, ?)''',
-            (site, ad, mu, sigma, alpha, n, norm))
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+            (site, ad, mu, mu_err, sigma, sigma_err, scale, scale_err, alpha,
+                alpha_err, norm, norm_err))
             conn.commit()
     lower_energy = mu - 3*sigma
     upper_energy = mu + 3*sigma
     # Now compute the efficiencies
     selection_cut = '{} && res_d >= {} && res_d <= {}'.format(selection_all,
             lower_energy, upper_energy)
-    selection_all_in_volume = 'target >= 0 && Volume_nCap == {}'
+    selection_all_in_volume = 'target >= 0 && Volume_nCap == {} && res_p > 1.5'
     selection_cut_in_volume = '{} && res_d >= {} && res_d <= {}'.format(
             selection_all_in_volume, lower_energy, upper_energy)
     def efficiency(selection, cut_selection):

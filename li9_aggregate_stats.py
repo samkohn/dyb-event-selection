f6de79180fc79473953c3e254b9618471073c628
Sam Kohn <kohn@berkeley.edu>
2020-08-03 14:58:58 -0700

    Update li9_aggregate_stats to connect to db

--- li9_aggregate_stats.py
+++ li9_aggregate_stats.py
@@ -9,7 +9,22 @@ import os
 import json
 import sqlite3
 
-def main2(files, site, outfile):
+def main2(files, site, outfile, db):
+    updated = aggregate(files, site)
+    with open(outfile, 'w') as f:
+        json.dump(updated, f)
+    if db is not None:
+        with sqlite3.Connection(db) as conn:
+            cursor = conn.cursor()
+            for key, value in updated['muon_counts'].items():
+                ntag = int('ntag' in key)
+                energy_class = key.split('_')[0]
+                cursor.execute('INSERT OR REPLACE INTO li9_muon_rates '
+                        'VALUES (?, ?, ?, ?, ?, ?, ?)',
+                        (site, energy_class, ntag, value, updated['daq_livetime'],
+                            value*1e9/updated['daq_livetime'], updated['usable_fraction']))
+
+def aggregate(files, site):
     daq_livetime = 0
     usable_livetime = 0
     num_veto_windows = 0
@@ -31,31 +46,23 @@ def main2(files, site, outfile):
                 raise
     efficiency = usable_livetime / daq_livetime
     rate = num_veto_windows*1e9/usable_livetime
-    with open(outfile, 'w') as f:
-        json.dump({
+    return {
             'site': site,
             'daq_livetime': daq_livetime,
             'usable_livetime': usable_livetime,
             'usable_fraction': efficiency,
             'num_veto_windows': num_veto_windows,
             'muon_counts': muon_counts,
-            }, f)
-    #if db is not None:
-        #with sqlite3.Connection(db) as conn:
-            #cursor = conn.cursor()
-            #cursor.execute('INSERT OR REPLACE INTO muon_rates '
-                    #'VALUES (?, ?, ?, ?, ?, ?)',
-                    #(run, ad, num_veto_windows, usable_livetime,
-                        #rate, efficiency))
+            }
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('files', nargs='+')
     parser.add_argument('--site', type=int)
     parser.add_argument('-o', '--output')
-    #parser.add_argument('--update-db',
-            #help='Optional, register the run info and muon rate with the given '
-            #'database')
+    parser.add_argument('--update-db',
+            help='Optional, register the run info and muon rate with the given '
+            'database')
     args = parser.parse_args()
-    main2(args.files, args.site, args.output)
+    main2(args.files, args.site, args.output, args.update_db)
 

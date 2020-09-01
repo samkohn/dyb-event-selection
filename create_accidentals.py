ea7d4ad6f6c7caefa98352dd3327e83da6e2efb1
Sam Kohn <kohn@berkeley.edu>
2020-09-01 12:57:49 -0700

    Update create_accidentals to use premade singles files

--- create_accidentals.py
+++ create_accidentals.py
@@ -14,7 +14,71 @@ def distance(a, b):
             + (a['y'] - b['y'])**2
             + (a['z'] - b['z'])**2)
 
-def main(infilename, outfile, ttree_name):
+def get_event(computed):
+    event = {
+            'energy': computed.energy,
+            'detector': computed.detector,
+            'x': computed.x,
+            'y': computed.y,
+            'z': computed.z,
+            }
+    return event
+
+def sequential_pairing(computed):
+    entries = computed.GetEntries()
+    halfway = entries//2
+    index = 0
+    first_events = []
+    second_events = []
+    while index < entries:
+        # Increment until we get a good single
+        computed.GetEntry(index)
+        event = get_event(computed)
+        if index < halfway:
+            first_events.append(event)
+        else:
+            second_events.append(event)
+        index += 1
+    return first_events, second_events
+
+def random_pairing_N(computed):
+    entries = computed.GetEntries()
+    halfway = entries//2
+    first_events = [None] * halfway
+    second_events = [None] * halfway
+    if entries % 2 == 0:
+        ordering = list(range(entries))
+    else:
+        ordering = list(range(entries - 1))
+    for entry, order in enumerate(ordering):
+        computed.GetEntry(entry)
+        event = get_event(computed)
+        if order % 2 == 0:
+            first_events[order // 2] = event
+        else:
+            second_events[order // 2] = event
+    return first_events, second_events
+
+def random_pairing_N_repeats(computed):
+    entries = computed.GetEntries()
+    halfway = entries//2
+    first_events = [None] * halfway
+    second_events = [None] * halfway
+    if entries % 2 == 0:
+        ordering = list(range(entries))
+    else:
+        ordering = list(range(entries - 1))
+    random.shuffle(ordering)
+    for entry, order in enumerate(ordering):
+        computed.GetEntry(entry)
+        event = get_event(computed)
+        if order % 2 == 0:
+            first_events[order // 2] = event
+        else:
+            second_events[order // 2] = event
+    return first_events, second_events
+
+def main(infilename, outfile, ttree_name, pairing_algorithm, repeat):
     import process
     import ROOT
     infile = ROOT.TFile(infilename, 'READ')
@@ -34,28 +98,30 @@ def main(infilename, outfile, ttree_name):
     computed.SetBranchStatus('x', 1)
     computed.SetBranchStatus('y', 1)
     computed.SetBranchStatus('z', 1)
-    entries = computed.GetEntries()
-    halfway = entries//2
-    index = 0
-    phase = 1
-    first_events = []
-    second_events = []
-    while index < entries:
-        # Increment until we get a good single
-        computed.GetEntry(index)
-        energy = computed.energy
-        event = {
-                'energy': energy,
-                'detector': computed.detector,
-                'x': computed.x,
-                'y': computed.y,
-                'z': computed.z,
-                }
-        if index < halfway:
-            first_events.append(event)
+    if pairing_algorithm == 'sequential':
+        first_events, second_events = sequential_pairing(computed)
+    elif pairing_algorithm == 'random_N':
+        if repeat > 1:
+            efficiency_list = []
+            for _ in range(repeat):
+                first_events, second_events = random_pairing_N(computed)
+                num_passes_DT = 0
+                for (first_event, second_event) in zip(first_events, second_events):
+                    # Compare event distances and fill the tree
+                    event_dr = distance(first_event, second_event)
+                    event_dt = random.randint(1000, delayeds._NH_THU_MAX_TIME)
+                    if delayeds.nH_THU_DT(event_dr, event_dt) < delayeds._NH_THU_DIST_TIME_MAX:
+                        num_passes_DT += 1
+                print(num_passes_DT)
+                efficiency = num_passes_DT / len(first_events)
+                print(efficiency)
+                efficiency_list.append(efficiency)
+            print(efficiency_list)
+            return
         else:
-            second_events.append(event)
-        index += 1
+            first_events, second_events = random_pairing_N(computed)
+    else:
+        raise NotImplemented(pairing_algorithm)
     for (first_event, second_event) in zip(first_events, second_events):
         # Compare event distances and fill the tree
         event_dr = distance(first_event, second_event)
@@ -91,6 +157,8 @@ if __name__ == '__main__':
     parser.add_argument('infile')
     parser.add_argument('outfile')
     parser.add_argument('--ttree-name', default='singles')
+    parser.add_argument('--pairing', default='sequential')
+    parser.add_argument('--repeat', type=int, default=-1)
     args = parser.parse_args()
 
-    main(args.infile, args.outfile, args.ttree_name)
+    main(args.infile, args.outfile, args.ttree_name, args.pairing, args.repeat)

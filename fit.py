efe86cd105f4edc598f54bebf83714813c162e04
Sam Kohn <kohn@berkeley.edu>
2020-09-20 17:25:54 -0700

    Update chi-square function to include all AD pairs and 1 bg pull term per AD

--- fit.py
+++ fit.py
@@ -1,26 +1,41 @@
 import argparse
+from pprint import pprint
 
 import numpy as np
 
-import prediction
+import prediction as pred
 
 
 def chi_square(constants, fit_params):
     """Compute the chi-square value for a single set of parameters.
-
-    Starting with just comparing EH1 to EH3, 1 set of reco bins.
     """
-    observed = np.zeros_like(constants.observed_spectra[(3, 1)])
-    for halldet in prediction.far_ads:
-        observed += constants.observed_spectra[halldet]
+    chi_square = 0
+    observed = {ad: constants.observed_candidates[ad] for ad in pred.far_ads}
+    predicted, reco_bins = pred.predict_ad_to_ad_obs(constants, fit_params)
+    pprint(observed)
+    pprint(predicted)
+    #Main part
+    for (far_halldet, near_halldet), n_predicted in predicted.items():
+        n_observed = observed[far_halldet]
+        sigma_observed = np.sqrt(n_observed)  # TODO placeholder error
+        numerator = np.power(n_observed - n_predicted, 2)
+        denominator = np.power(sigma_observed, 2)
+        chi_square += np.sum(numerator/denominator)  # sum over energy bins
 
-    predicted = prediction.predict_halls(constants, fit_params)[1]
+    # Pull terms
+    for halldet, pull in fit_params.pull_bg.items():
+        numerator = pull*pull
+        denominator = 245 * 245  # TODO placeholder error
+        chi_square += numerator/denominator
+    return chi_square
+
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("database")
     args = parser.parse_args()
-    constants = prediction.default_constants(args.database)
-    starting_params = prediction.FitParams(0.15, 2.5e-3)
+    constants = pred.default_constants(args.database)
+    starting_params = pred.FitParams(0.15, 2.5e-3, pred.ad_dict(0))
     print(chi_square(constants, starting_params))

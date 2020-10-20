a8c25667c8e411187464e95fc2573208320e958f
Sam Kohn <kohn@berkeley.edu>
2020-10-20 10:35:20 -0700

    Add script to automate loading of emitted reactor spectra
    This is the LBNL fitter/ToyMC pre-computed spectra averaged over a data
    period and includes non-equilibrium and SNF corrections.

---
+++ reactor_spectra.py
@@ -0,0 +1,30 @@
+"""Read and store the reactor spectra."""
+import argparse
+import sqlite3
+
+def main(infilename, database, source, data_period):
+    with open(infilename, 'r') as f:
+        with sqlite3.Connection(database) as conn:
+            cursor = conn.cursor()
+            for line in f:
+                items = [float(x) for x in line.split()]
+                energy = items[0]
+                for core_index, value in enumerate(items[1:]):
+                    cursor.execute('''
+                        INSERT OR REPLACE INTO reactor_emitted_spectrum
+                        VALUES (?, ?, ?, ?, ?)''',
+                        (energy, core_index + 1, value, data_period, source)
+                    )
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile')
+    parser.add_argument('database')
+    parser.add_argument('source')
+    parser.add_argument('data_period', choices=('6ad', '8ad', '7ad'))
+    args = parser.parse_args()
+    print(args)
+    main(args.infile, args.database, args.source, args.data_period)

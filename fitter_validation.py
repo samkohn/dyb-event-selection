2ae010c88931c7241c64dd69b87963853bc38df3
Sam Kohn <kohn@berkeley.edu>
2020-10-25 17:21:07 -0700

    Update fitter_validation with easier rate/rate+shape

--- fitter_validation.py
+++ fitter_validation.py
@@ -38,7 +38,7 @@ def main(database, label, toy_out_location, toy_code_dir, config_template, find_
     #sin2_values = np.array([0.065])
     #dm2ee_values = np.array([0.0027])
     dm2ee_values = np.linspace(2.3e-3, 2.7e-3, 6)
-    source_template = "LBNL ToyMC 05 w/ full fluctuations default minus lowest nGd binning sin2={sin2} dm2ee={dm2ee} experiment #{entry}"
+    source_template = "LBNL ToyMC 05 w/ full fluctuations default nGd binning sin2={sin2} dm2ee={dm2ee} experiment #{entry}"
     fit_config = {
         #"database": "/home/skohn/parameters_new.db",
         "database": "/global/cscratch1/sd/skohn/dyb30/parameters.db",
@@ -48,7 +48,7 @@ def main(database, label, toy_out_location, toy_code_dir, config_template, find_
         "mult_eff": [0.9770, 0.9774, 0.9782, 0.9781, 0.9785, 0.9787, 0.9785, 0.9784],
         "muon_eff": [0.8233, 0.8196, 0.8516, 0.8501, 0.9832, 0.9830, 0.9828, 0.9830],
         "masses": [19941, 19966, 19891, 19945, 19913, 19991, 19892, 19931],
-        "num_coincs": True,
+        "num_coincs": "/global/cscratch1/sd/skohn/dyb30/fitter_validation.db",
         "num_coincs_source": None, # This is what we'll replace in the loop
         "reco_bins": None,
         "det_response_source": "LBNL ToyMC directly from Matt nominal reco binning, adapted true binning",
@@ -64,6 +64,35 @@ def main(database, label, toy_out_location, toy_code_dir, config_template, find_
         'deltaMSqee       2.48e-3', 'deltaMSqee       {dm2ee}'
     )
 
+    #with multiprocessing.Pool(63) as pool:
+        #results = pool.starmap(run_validation_on_experiment, [(
+                #label,
+                #generate_toy(
+                    #toy_out_location,
+                    #toy_code_dir,
+                    #toy_config_template.format(sin2=sin2, dm2ee=dm2ee),
+                    #sin2,
+                    #dm2ee
+                #),
+                #0,
+                #i,
+                #database,
+                #source_template,
+                #fit_config,
+                #fit_file_name,
+                #sin2,
+                #dm2ee,
+                #find_errors,
+                #False
+            #) for i, (sin2, dm2ee) in enumerate(itertools.product(sin2_values, dm2ee_values))
+        #])
+    #with sqlite3.Connection(database) as conn:
+        #cursor = conn.cursor()
+        #cursor.executemany('''INSERT OR REPLACE INTO fitter_validation_results
+        #VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', results)
+    #return
+
+
     for i, (sin2, dm2ee) in enumerate(itertools.product(sin2_values, dm2ee_values)):
         #range(0, 1000, 4))):  # ToyMC fluctuations change every 4?
         print(sin2, dm2ee)
@@ -77,13 +106,13 @@ def main(database, label, toy_out_location, toy_code_dir, config_template, find_
                 entry,
                 database,
                 source_template.format(sin2=sin2, dm2ee=dm2ee, entry=entry),
-                "default minus lowest",
+                "default",
             )
-        with multiprocessing.Pool(60) as pool:
+        with multiprocessing.Pool(63) as pool:
             results = pool.starmap(run_validation_on_experiment, [(
                 label, toyfilename, entry, i*len(entries) + j, database,
                 source_template, fit_config, fit_file_name, sin2,
-                dm2ee, find_errors) for j, entry in enumerate(entries)])
+                dm2ee, find_errors, True) for j, entry in enumerate(entries)])
         with sqlite3.Connection(database) as conn:
             cursor = conn.cursor()
             cursor.executemany('''INSERT OR REPLACE INTO fitter_validation_results
@@ -91,7 +120,7 @@ def main(database, label, toy_out_location, toy_code_dir, config_template, find_
 
 
 def run_validation_on_experiment(label, toyfilename, entry, index, database,
-        source_template, fit_config, fit_file_name, sin2, dm2ee, find_errors):
+        source_template, fit_config, fit_file_name, sin2, dm2ee, find_errors, rate_only):
     print(index)
     fit_config['num_coincs_source'] = source_template.format(sin2=sin2,
             dm2ee=dm2ee, entry=entry)
@@ -99,23 +128,31 @@ def run_validation_on_experiment(label, toyfilename, entry, index, database,
     with open(fit_file_name, 'w') as f:
         json.dump(fit_config, f)
     constants = pred.load_constants(fit_file_name)
-    constants.input_osc_params.m2_ee = dm2ee
+    if rate_only:
+        starting_dm2 = dm2ee
+    else:
+        starting_dm2 = 2.48e-3
     starting_params = pred.FitParams(
             0.15,
-            2.48e-3,
+            starting_dm2,
             pred.ad_dict(0),
             pred.ad_dict(0, halls='near'),
             pred.core_dict(0),
             pred.ad_dict(0),
     )
     near_ads = None
-    frozen_params = range(2, 10)
+    if rate_only:
+        frozen_params = range(1, 10)
+    else:
+        frozen_params = range(2, 10)
     result = fit.fit_lsq_frozen(starting_params, constants, frozen_params,
-            near_ads=near_ads, rate_only=False)
-    fit_params = pred.FitParams.from_list(
-            [result.x[0], result.x[1]] + [0] * 8 + result.x[2:].tolist()
-    )
-    chi2_min = fit.chi_square(constants, fit_params, rate_only=False)
+            near_ads=near_ads, rate_only=rate_only)
+    if rate_only:
+        param_list = [result.x[0], dm2ee] + [0] * 8 + result.x[1:].tolist()  # rate-only
+    else:
+        param_list = [result.x[0], result.x[1]] + [0] * 8 + result.x[2:].tolist()
+    fit_params = pred.FitParams.from_list(param_list)
+    chi2_min = fit.chi_square(constants, fit_params, rate_only=rate_only)
     best_sin2 = np.power(np.sin(2*fit_params.theta13), 2)
     dm2ee_best = fit_params.m2_ee
     if find_errors:
@@ -132,7 +169,7 @@ def run_validation_on_experiment(label, toyfilename, entry, index, database,
     #time.sleep(random.uniform(0, 10))
     os.remove(fit_file_name)
     return (label, index, sin2, best_sin2, sin2_error, dm2ee, dm2ee_best, dm2ee_error,
-            chi2_min, True)
+            chi2_min, rate_only)
     with sqlite3.Connection(database, timeout=20) as conn:
         cursor = conn.cursor()
         cursor.execute('''INSERT OR REPLACE INTO fitter_validation_results

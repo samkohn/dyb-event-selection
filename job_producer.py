51736f1b3ff7e0c3419a313c0a848291e5b2b0ac
Sam Kohn <kohn@berkeley.edu>
2020-12-20 22:35:08 -0800

    Update job_producer to add an if __name__ == '__main__' guard

--- job_producer.py
+++ job_producer.py
@@ -47,39 +47,40 @@ def generate_worker_command(line, command_template, run_only, per_ad):
         command = command_template.format(**args)
         return command
 
-parser = argparse.ArgumentParser(epilog=
-        '''Command template variables:
-  - {filepath} = path to input file
-  - {run} = run number, as an int
-  - {fileno} = sub-run / file number, as an int
-  - {site} = site / hall number (1, 2, or 3)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(epilog=
+            '''Command template variables:
+      - {filepath} = path to input file
+      - {run} = run number, as an int
+      - {fileno} = sub-run / file number, as an int
+      - {site} = site / hall number (1, 2, or 3)
 
-The command template will be passed to python's string.format function
-so the template can contain more detailed formatting instructions (such
-as zero-padding the fileno using {fileno:>04}).''',
-formatter_class=argparse.RawDescriptionHelpFormatter)
+    The command template will be passed to python's string.format function
+    so the template can contain more detailed formatting instructions (such
+    as zero-padding the fileno using {fileno:>04}).''',
+    formatter_class=argparse.RawDescriptionHelpFormatter)
 
-parser.add_argument('runlist')
-parser.add_argument('ntasks', type=int)
-parser.add_argument('--start-task', type=int, default=1)
-parser.add_argument('command', metavar='COMMAND_TEMPLATE')
-parser.add_argument('--run-only', action='store_true')
-parser.add_argument('--per-ad', action='store_true')
-args = parser.parse_args()
-runlist = args.runlist
-ntasks = args.ntasks
-start_task = args.start_task
-command_template = args.command
-with open(runlist, 'r') as fin:
-    for i, line in enumerate(fin):
-        if i < start_task - 1:
-            continue
-        if i >= ntasks + start_task - 1:
-            break
-        command = generate_worker_command(line, command_template,
-                args.run_only, args.per_ad)
-        if args.per_ad:
-            for c in command:
-                print(c)
-        else:
-            print(command)
+    parser.add_argument('runlist')
+    parser.add_argument('ntasks', type=int)
+    parser.add_argument('--start-task', type=int, default=1)
+    parser.add_argument('command', metavar='COMMAND_TEMPLATE')
+    parser.add_argument('--run-only', action='store_true')
+    parser.add_argument('--per-ad', action='store_true')
+    args = parser.parse_args()
+    runlist = args.runlist
+    ntasks = args.ntasks
+    start_task = args.start_task
+    command_template = args.command
+    with open(runlist, 'r') as fin:
+        for i, line in enumerate(fin):
+            if i < start_task - 1:
+                continue
+            if i >= ntasks + start_task - 1:
+                break
+            command = generate_worker_command(line, command_template,
+                    args.run_only, args.per_ad)
+            if args.per_ad:
+                for c in command:
+                    print(c)
+            else:
+                print(command)

cc098bd08e0a209ee520fac0c23ebf11099e473b
Sam Kohn <kohn@berkeley.edu>
2020-12-23 13:13:43 -0800

    Add copies of some scripts with AdTime modification

---
+++ compute_dist_time_eff_adtime.py
@@ -0,0 +1,110 @@
+import argparse
+import math
+import os
+import sqlite3
+
+def get_stat_error(n_passes_cut, n_fails_cut, error_passes, error_fails):
+    """Compute the statistical error on the efficiency.
+
+    This function performs error propagation on the expression:
+
+        Efficiency = N(passes DT cut) / N(total)
+
+    including the partial correlation between the numerator and the
+    denominator. This is accomplished by assigning:
+
+        N(total) = N(passes) + N(fails)
+
+    Then rewriting the expression as:
+
+        Efficiency = 1 / (1 + N(fails) / N(passes))
+
+    This expression now has no hidden statistical correlations due to the same
+    histogram bin being counted twice, which the original expression does have.
+    The final mathematical expression is:
+
+        Error = ...
+            N(fails)/N(passes) * Error(N(fails)/N(passes)) / (1 + fails/passes)^2
+    """
+    value_ratio = n_fails_cut / n_passes_cut
+    passes_relative_error_sq = pow(error_passes / n_passes_cut, 2)
+    fails_relative_error_sq = pow(error_fails / n_fails_cut, 2)
+    quotient_propagation = math.sqrt(passes_relative_error_sq +
+            fails_relative_error_sq)
+    power_term = pow(1 + value_ratio, 2)
+    return value_ratio * quotient_propagation/power_term
+
+def main(input_basepath, database, update_db):
+    import ROOT
+    with sqlite3.Connection(database) as conn:
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute('''SELECT Hall, DetNo, Peak, Resolution
+        FROM delayed_energy_fits WHERE Hall > 0 AND DetNo > 0
+        AND Source = "adtime"''')
+        results = cursor.fetchall()
+    cut_lookup = {(row['Hall'], row['DetNo']): row for row in results}
+    effs = {}
+    stat_errors = {}
+    binning_errors = {}
+    DT_LOW_BIN = 1
+    DT_CUT_UP_BIN = 16  # 800mm
+    DT_ALL_UP_BIN = 60  # 3000mm
+    for (site, det), cuts in cut_lookup.items():
+        site_dir = 'EH{}'.format(site)
+        sub_dir = 'sub_using_adtime_ad{}'.format(det)
+        name = 'sub_ad{}.root'.format(det)
+        path = os.path.join(input_basepath, site_dir, sub_dir, name)
+        print(path)
+        infile = ROOT.TFile(path, 'READ')
+        delayed_vs_DT = infile.Get('ed_DT_sub')
+        low_limit = cuts['Peak'] - 3 * cuts['Resolution']
+        up_limit = cuts['Peak'] + 3 * cuts['Resolution']
+        print(low_limit, up_limit)
+        energy_low_bin = delayed_vs_DT.GetYaxis().FindBin(low_limit)
+        energy_up_bin = delayed_vs_DT.GetYaxis().FindBin(up_limit)
+        cut_error = ROOT.Double()
+        only_excluded_error = ROOT.Double()
+        cut_integral = delayed_vs_DT.IntegralAndError(DT_LOW_BIN, DT_CUT_UP_BIN,
+                energy_low_bin, energy_up_bin, cut_error)
+        all_integral = delayed_vs_DT.Integral(DT_LOW_BIN, DT_ALL_UP_BIN,
+                energy_low_bin, energy_up_bin)
+        only_excluded_integral = delayed_vs_DT.IntegralAndError(DT_CUT_UP_BIN+1,
+                DT_ALL_UP_BIN, energy_low_bin, energy_up_bin,
+                only_excluded_error)
+        stat_error = get_stat_error(cut_integral, only_excluded_integral,
+                cut_error.real, only_excluded_error.real)
+        bin_width_error_integral = delayed_vs_DT.Integral(DT_LOW_BIN,
+                DT_CUT_UP_BIN, energy_low_bin+1, energy_up_bin-1)
+        efficiency = cut_integral/all_integral
+        bin_width_changes = bin_width_error_integral/all_integral
+        bin_width_error = (efficiency - bin_width_changes)/2
+
+        print(f'EH{site}-AD{det}')
+        print(f'Nominal: {100*efficiency:.02f}%')
+        print(f'Deviation due to 0.005MeV bin width: {100*bin_width_error:.03f}%')
+        print(f'Statistical error: {100*stat_error:.03f}%')
+        print(f'N(passes): {cut_integral} +/- {cut_error.real}')
+        print(f'N(fails):  {only_excluded_integral} +/- {only_excluded_error.real}')
+        print(f'N(total):  {all_integral}')
+        infile.Close()
+
+        effs[site, det] = efficiency
+        stat_errors[site, det] = stat_error
+        binning_errors[site, det] = bin_width_error
+    if update_db:
+        with sqlite3.Connection(database) as conn:
+            cursor = conn.cursor()
+            for (site, det), eff in effs.items():
+                cursor.execute('''INSERT OR REPLACE INTO
+                distance_time_cut_efficiency VALUES
+                (?, ?, ?, ?, ?, ?)''', (site, det, "adtime", eff, stat_errors[site, det],
+                    binning_errors[site, det]))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='directory containing EH1, EH2, EH3')
+    parser.add_argument('database')
+    parser.add_argument('--update-db', action='store_true')
+    args = parser.parse_args()
+    main(args.input, args.database, args.update_db)
---
+++ fit_delayed_adtime.py
@@ -0,0 +1,132 @@
+import argparse
+import math
+import sqlite3
+import time
+
+def crystal_ball(x, p):
+    """Crystal Ball function.
+
+    x (input data) is 1-dimensional, i.e. len(x) == 1
+
+    p (parameters) has 5 entries:
+      p[0] = mean of gaussian
+      p[1] = variance of gaussian
+      p[2] = cutoff between peak and tail
+      p[3] = power of the power-law tail
+      p[4] = normalization
+    """
+    inval = x[0]
+    mu, sigma, alpha, n, norm = p
+    if alpha < 0:
+        alpha = -alpha
+    scaled = (inval - mu)/sigma
+    if scaled > -alpha:
+        return norm * math.exp(-scaled*scaled/2)
+    else:
+        return norm * (n/alpha)**n * math.exp(-alpha*alpha/2) * (
+                n/alpha - alpha - scaled)**(-n)
+
+def calorimeter_fn(x, p):
+    """Calorimeter function.
+
+    x (input data) is 1-dimensional, i.e. len(x) == 1, x == [value].
+
+    p (parameters) has 5 entries:
+      p[0] = true peak energy (~mu)
+      p[1] = energy resolution (~sigma)
+      p[2] = scale of exponential (~lambda, i.e. lam*exp(lam*E))
+      p[3] = fraction of events in true peak (~alpha)
+      p[4] = normalization
+    """
+    try:
+        inval = x[0]
+        peak, resolution, exp_scale, peak_fraction, norm = p
+        erf = math.erf
+        exp = math.exp
+        sqrt2 = math.sqrt(2)
+        sqrt2pi = math.sqrt(2*math.pi)
+        peak_norm = peak_fraction / (resolution * sqrt2pi)
+        peak_exp = exp(-(inval - peak)**2/(2 * resolution**2))
+        peak_term = peak_norm * peak_exp
+
+        tail_coeff = (1 - peak_fraction) * exp_scale / (exp(exp_scale * peak) - 1)
+        tail_exp = exp(((resolution * exp_scale)**2 + 2 * exp_scale * inval)/2)
+        tail_erf1 = erf((peak - inval - resolution**2 * exp_scale) / (sqrt2 *
+            resolution))
+        tail_erf2 = erf((-inval - resolution**2 * exp_scale) / (sqrt2 *
+            resolution))
+        tail_term = tail_coeff * tail_exp * (tail_erf1 - tail_erf2)
+        return norm * (peak_term + tail_term)
+    except:
+        print([y for y in x])
+        print([y for y in p])
+        raise
+
+
+def main(infilename, outfilename, site, ad, source, database):
+    import ROOT
+    infile = ROOT.TFile(infilename, 'READ')
+    spectrum_2d = infile.Get('final')
+    delayed_spectrum = spectrum_2d.ProjectionY()
+    mu0 = 2.3
+    sigma0 = .14
+    alpha0 = 0.8
+    scale0 = 2
+    # Approximate the normalization as the area under the main part of
+    # the nH peak (1.5-3.5 MeV)
+    low_bin = delayed_spectrum.FindFixBin(1.5)
+    up_bin = delayed_spectrum.FindFixBin(3.499)
+    approx_integral = delayed_spectrum.Integral(low_bin, up_bin)
+    bin_width = delayed_spectrum.GetBinWidth(1)
+    norm0 = approx_integral * bin_width
+
+    fitter = ROOT.TF1("calo_fitter", calorimeter_fn, 1.5, 12, 5)
+    fitter.SetParameters(mu0, sigma0, scale0, alpha0, norm0)
+
+    if outfilename is None:
+        options = 'QN0S'
+    else:
+        options = 'QS'
+    try:
+        fit_result = delayed_spectrum.Fit(fitter, options, '', 1.6, 2.8)
+    except:
+        print([mu0, sigma0, scale0, alpha0, norm0])
+        delayed_spectrum.Draw()
+        fitter.Draw()
+        time.sleep(10)
+        ROOT.gPad.Print('error.pdf')
+        raise
+    mu, sigma, scale, alpha, norm = [fit_result.Parameter(i) for i in range(5)]
+    mu_err, sigma_err, scale_err, alpha_err, norm_err = [fit_result.ParError(i)
+            for i in range(5)]
+    chi_square = fit_result.Chi2()
+    num_bins = fitter.GetNumberFitPoints()
+    num_params = fitter.GetNpar()
+    delayed_spectrum.GetXaxis().SetRangeUser(1.5, 3.4)
+    if outfilename is not None:
+        ROOT.gPad.Print(outfilename)
+    infile.Close()
+    if database is not None:
+        with sqlite3.Connection(database) as conn:
+            c = conn.cursor()
+            c.execute('''INSERT OR REPLACE INTO delayed_energy_fits
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+            (site, ad, source, mu, mu_err, sigma, sigma_err, scale, scale_err, alpha,
+                alpha_err, norm, norm_err, chi_square, num_bins, num_params))
+            conn.commit()
+    else:
+        print(mu, sigma, scale, alpha, norm)
+        print(mu_err, sigma_err, scale_err, alpha_err, norm_err)
+        print(f'Chi2/NDF: {chi_square:.03f} / ({num_bins} - {num_params})')
+    return
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input')
+    parser.add_argument('-o', '--output', default=None)
+    parser.add_argument('--site', type=int)
+    parser.add_argument('--ad', type=int)
+    parser.add_argument('--source', help='Label for database')
+    parser.add_argument('--update-db', default=None)
+    args = parser.parse_args()
+    main(args.input, args.output, args.site, args.ad, args.source, args.update_db)

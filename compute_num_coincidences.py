54e77daa036c4cb162dcaf936c97af5529a6ff9a
Sam Kohn <kohn@berkeley.edu>
2020-12-23 11:01:38 -0800

    Update compute_num_coincidences to use separate energy cuts for adtime

--- compute_num_coincidences.py
+++ compute_num_coincidences.py
@@ -13,12 +13,13 @@ def one_file(run_key, data_file_path, energy_lookup):
     data_file_name = data_file_path.format(run=run, site=site, ad=ad)
     data_file = ROOT.TFile(data_file_name, 'READ')
     ad_events = data_file.Get('ad_events')
-    delayed_min, delayed_max = energy_lookup[site, ad]
+    delayed_min, delayed_max = energy_lookup['nominal', site, ad]
     num_coincidences_nominal = ad_events.Draw('energy[0]',
         f'energy[1] > {delayed_min} && energy[1] < {delayed_max} && '
         f'{delayeds._NH_THU_DIST_TIME_CUT_STR}',
         'goff'
     )
+    delayed_min, delayed_max = energy_lookup['adtime', site, ad]
     num_coincidences_adtime = ad_events.Draw('energy[0]',
         f'energy[1] > {delayed_min} && energy[1] < {delayed_max} && '
         f'dr_to_prompt_AdTime[1] + {delayeds._NH_THU_DIST_TIME_CONST} '
@@ -42,18 +43,27 @@ def main(main_database, energy_cuts_database, data_file_path, update_db):
         cursor = conn.cursor()
         cursor.execute('''SELECT Hall, DetNo, Peak - 3 * Resolution,
             Peak + 3 * Resolution
-            FROM delayed_energy_fits''')
-        energy_bounds = cursor.fetchall()
+            FROM delayed_energy_fits
+            WHERE Source = "nominal"''')
+        nominal_energy_bounds = cursor.fetchall()
+        cursor.execute('''SELECT Hall, DetNo, Peak - 3 * Resolution,
+            Peak + 3 * Resolution
+            FROM delayed_energy_fits
+            WHERE Source = "adtime"''')
+        adtime_energy_bounds = cursor.fetchall()
+
     energy_lookup = {}
-    for site, ad, low_bound, up_bound in energy_bounds:
-        energy_lookup[site, ad] = (low_bound, up_bound)
+    for site, ad, low_bound, up_bound in nominal_energy_bounds:
+        energy_lookup['nominal', site, ad] = (low_bound, up_bound)
+    for site, ad, low_bound, up_bound in adtime_energy_bounds:
+        energy_lookup['adtime', site, ad] = (low_bound, up_bound)
 
     with multiprocessing.Pool(63) as pool:
         results = pool.starmap(one_file, zip(run_keys, it.repeat(data_file_path),
             it.repeat(energy_lookup)))
     if update_db:
-        results_nominal = [(run, ad, num_nominal, 'nominal') for run, ad, num_nominal, _ in results]
-        results_adtime = [(run, ad, num_adtime, 'adtime') for run, ad, _, num_adtime in results]
+        results_nominal = [(run, ad, num_nominal, 'new nominal') for run, ad, num_nominal, _ in results]
+        results_adtime = [(run, ad, num_adtime, 'new adtime') for run, ad, _, num_adtime in results]
         with sqlite3.Connection(main_database) as conn:
             cursor = conn.cursor()
             cursor.executemany('''INSERT OR REPLACE INTO num_coincidences_by_run

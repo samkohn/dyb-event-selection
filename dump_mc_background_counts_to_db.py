1884259f88858a73102f735db3bb3b3fd87599be
Sam Kohn <kohn@berkeley.edu>
2021-01-04 13:02:34 -0800

    Update MC background dumper to store all rates and errors

--- dump_mc_background_counts_to_db.py
+++ dump_mc_background_counts_to_db.py
@@ -3,11 +3,27 @@ import argparse
 
 import common
 
-def main(infilename, database, bg_index, label):
+BG_LOOKUP = {
+    '11': 'accidental',
+    '13': 'li9',
+    '15': 'fast-neutron',
+    '17': 'amc',
+}
+
+BG_ERROR_LOOKUP = {
+    '12': 'accidental',
+    '14': 'li9',
+    '16': 'fast-neutron',
+    '18': 'amc',
+}
+
+def main(infilename, database, label):
 
     livetime_index = '2'
     muon_eff_index = '3'
     mult_eff_index = '4'
+    bg_values = {}
+    bg_errors = {}
     with open(infilename, 'r') as infile:
         for line in infile:
             if line[0] == '#' or len(line) < 10:
@@ -19,33 +35,43 @@ def main(infilename, database, bg_index, label):
                 muon_effs = [float(val) for val in splits[2:]]
             if splits[1] == mult_eff_index:
                 mult_effs = [float(val) for val in splits[2:]]
-            if splits[1] == str(bg_index):
+            if splits[1] in BG_LOOKUP:
                 bg_rates = [float(val) for val in splits[2:]]
-                break
-        else:  # nobreak
-            raise ValueError(f"Couldn't find background index {bg_index}")
+                bg_values[BG_LOOKUP[splits[1]]] = bg_rates
+            if splits[1] in BG_ERROR_LOOKUP:
+                bg_error = [float(val) for val in splits[2:]]
+                bg_errors[BG_ERROR_LOOKUP[splits[1]]] = bg_error
     with common.get_db(database) as conn:
         cursor = conn.cursor()
-        for (hall, det), livetime, mu_eff, mult_eff, bg_rate in zip(
-            common.all_ads, livetimes, muon_effs, mult_effs, bg_rates
-        ):
-            cursor.execute('''
-                INSERT OR REPLACE INTO
-                    accidentals_counts
-                VALUES
-                    (?, ?, ?, ?)
-                ''',
-                (label, hall, det, livetime * bg_rate * mu_eff * mult_eff),
-            )
+        for i, ((hall, det), livetime, mu_eff, mult_eff) in enumerate(zip(
+            common.all_ads, livetimes, muon_effs, mult_effs
+        )):
+            for bg_type in bg_values:
+                bg_rate = bg_values[bg_type][i]
+                bg_error = bg_errors[bg_type][i]
+                cursor.execute('''
+                    INSERT OR REPLACE INTO
+                        bg_counts
+                    VALUES
+                        (?, ?, ?, ?, ?, ?)
+                    ''',
+                    (
+                        label,
+                        hall,
+                        det,
+                        bg_type,
+                        livetime * bg_rate * mu_eff * mult_eff,
+                        livetime * bg_error * mu_eff * mult_eff
+                    ),
+                )
     return
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('infile', help='Theta13-etc. ToyMC file')
     parser.add_argument('database')
-    parser.add_argument('--bg-index', type=int, help='"row" number in input file')
     parser.add_argument('--label')
     args = parser.parse_args()
-    main(args.infile, args.database, args.bg_index, args.label)
+    main(args.infile, args.database, args.label)
 
 

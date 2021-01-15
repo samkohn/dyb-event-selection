d92976eb2fb96c25150b61e55cea7cf126238efc
Sam Kohn <kohn@berkeley.edu>
2021-01-14 23:54:42 -0800

    Save spectra of adtime vs adsimple coincidences breakdown

--- compute_adtime_coinc_breakdown.py
+++ compute_adtime_coinc_breakdown.py
@@ -2,8 +2,11 @@
 which reconstruction's distance-time cut is satisfied."""
 
 import argparse
+from collections import defaultdict
 import itertools as it
 import multiprocessing
+
+import numpy as np
 
 import common
 import delayeds
@@ -13,6 +16,9 @@ ADTIME_DT_CUT = (
     f'(dr_to_prompt_AdTime[1] + ({delayeds._NH_THU_DIST_TIME_CONST} '
     f'* dt_to_prompt[1]) < {delayeds._NH_THU_DIST_TIME_MAX})'
 )
+
+HIST_NBINS = 105
+HIST_BINNINGID = 4
 
 def one_file(run_key, data_file_path, energy_lookup):
     import ROOT
@@ -24,14 +30,13 @@ def one_file(run_key, data_file_path, energy_lookup):
     ad_events = data_file.Get('ad_events')
     delayed_min_adsimple, delayed_max_adsimple = energy_lookup['nominal', site, ad]
     delayed_min_adtime, delayed_max_adtime = energy_lookup['adtime', site, ad]
-    #HIST_NBINS = 105
-    #hists_for_ad = (
-        #ROOT.TH1F(f'both_eh{site}ad{ad}', f'both_eh{site}ad{ad}', HIST_NBINS, 1.5, 12),
-        #ROOT.TH1F(f'adsimple_only_eh{site}ad{ad}', f'adsimple_only_eh{site}ad{ad}',
-            #HIST_NBINS, 1.5, 12)
-        #ROOT.TH1F(f'adtime_only_eh{site}ad{ad}', f'adtime_only_eh{site}ad{ad}',
-            #HIST_NBINS, 1.5, 12)
-    #)
+    hists_for_ad = (
+        ROOT.TH1F(f'both_eh{site}ad{ad}', f'both_eh{site}ad{ad}', HIST_NBINS, 1.5, 12),
+        ROOT.TH1F(f'adsimple_only_eh{site}ad{ad}', f'adsimple_only_eh{site}ad{ad}',
+            HIST_NBINS, 1.5, 12),
+        ROOT.TH1F(f'adtime_only_eh{site}ad{ad}', f'adtime_only_eh{site}ad{ad}',
+            HIST_NBINS, 1.5, 12),
+    )
     PASSES_ADSIMPLE = (
         f'(energy[1] > {delayed_min_adsimple} && '
         f'energy[1] < {delayed_max_adtime} && '
@@ -66,14 +71,14 @@ def one_file(run_key, data_file_path, energy_lookup):
     )
     #print('num_passes_neither:', num_passes_neither)
     # Construct return arrays
-    #both_histo = []
-    #adsimple_histo = []
-    #adtime_histo = []
-    #both, adsimple, adtime = hists_for_ad
-    #for i in range(HIST_NBINS):
-        #both_histo.append(both.GetBinContent(i + 1))
-        #adsimple_histo.append(adsimple.GetBinContent(i + 1))
-        #adtime_histo.append(adtime.GetBinContent(i + 1))
+    both_histo = []
+    adsimple_histo = []
+    adtime_histo = []
+    both, adsimple, adtime = hists_for_ad
+    for i in range(HIST_NBINS):
+        both_histo.append(both.GetBinContent(i + 1))
+        adsimple_histo.append(adsimple.GetBinContent(i + 1))
+        adtime_histo.append(adtime.GetBinContent(i + 1))
     return (
         run,
         ad,
@@ -81,9 +86,9 @@ def one_file(run_key, data_file_path, energy_lookup):
         num_passes_only_adsimple,
         num_passes_only_adtime,
         num_passes_neither,
-        #both_histo,
-        #adsimple_histo,
-        #adtime_histo,
+        both_histo,
+        adsimple_histo,
+        adtime_histo,
     )
 
 def main(database, data_file_path, update_db):
@@ -116,13 +121,64 @@ def main(database, data_file_path, update_db):
     with multiprocessing.Pool() as pool:
         results = pool.starmap(one_file, zip(run_keys, it.repeat(data_file_path),
             it.repeat(energy_lookup)))
+    # Set up dicts to hold combined histograms
+    # The first time a (site, ad) pair is encountered it will produce 0 (int() == 0)
+    # and numpy will add the array to 0 to produce the first stored array.
+    both_hist = defaultdict(int)
+    adsimple_hist = defaultdict(int)
+    adtime_hist = defaultdict(int)
+    # Combine result histograms
+    for (run, site, ad), result in zip(run_keys, results):
+        both_hist[site, ad] += np.array(result[6])
+        adsimple_hist[site, ad] += np.array(result[7])
+        adtime_hist[site, ad] += np.array(result[8])
     if update_db:
         # add Label field
-        rows = [(*result, '1/11/21') for result in results]
+        #result_rows = [(*result[:6], '1/11/21') for result in results]
+        spectrum_rows = []
+        for (site, ad), spectrum in both_hist.items():
+            for i, val in enumerate(spectrum):
+                spectrum_rows.append((
+                    site,
+                    ad,
+                    'both 1/11/21',
+                    HIST_BINNINGID,
+                    i,
+                    val,
+                ))
+        for (site, ad), spectrum in adsimple_hist.items():
+            for i, val in enumerate(spectrum):
+                spectrum_rows.append((
+                    site,
+                    ad,
+                    'adsimple only 1/11/21',
+                    HIST_BINNINGID,
+                    i,
+                    val,
+                ))
+        for (site, ad), spectrum in adtime_hist.items():
+            for i, val in enumerate(spectrum):
+                spectrum_rows.append((
+                    site,
+                    ad,
+                    'adtime only 1/11/21',
+                    HIST_BINNINGID,
+                    i,
+                    val,
+                ))
         with common.get_db(database) as conn:
             cursor = conn.cursor()
             cursor.executemany('''INSERT OR REPLACE INTO adtime_DT_cut_breakdown
-                VALUES (?, ?, ?, ?, ?, ?, ?)''', rows)
+                VALUES (?, ?, ?, ?, ?, ?, ?)''', result_rows)
+            cursor.executemany('''
+                INSERT OR REPLACE INTO
+                    adtime_DT_cut_breakdown_spectra
+                VALUES
+                    (?, ?, ?, ?, ?, ?)
+                ''',
+                spectrum_rows,
+            )
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()

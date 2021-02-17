a0d94939e4d6b11dbac04933f393bc07f76552c0
Sam Kohn <kohn@berkeley.edu>
2021-02-16 22:19:28 -0800

    Add very beginning of dump_fit_inputs

---
+++ dump_fit_inputs.py
@@ -0,0 +1,12 @@
+"""Create a portable representation of fitter inputs for sharing."""
+
+import argparse
+import json
+
+import ROOT
+import numpy as np
+
+import prediction as pred
+
+if __name__ == '__main__':
+

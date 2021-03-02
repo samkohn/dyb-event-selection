b043868ca6e88d09be05c3b1e1525e76289a006e
Sam Kohn <kohn@berkeley.edu>
2021-03-02 13:59:20 -0800

    Add unvetoed livetime, muon rate, and singles rate lookups

--- compute_dataset_summary.py
+++ compute_dataset_summary.py
@@ -34,6 +34,32 @@ def daq_livetime_days(database, label):
     livetime_s = daq_livetime_s(database, label)
     return livetime_s/60/60/24
 
+def unvetoed_livetime_s(database, label):
+    """Return an array of unvetoed livetimes ordered from EH1-AD1 to EH3-AD4."""
+    with common.get_db(database) as conn:
+        cursor = conn.cursor()
+        # Total DAQ Livetime
+        cursor.execute('''
+            SELECT
+                SUM(Livetime_ns/1e9)
+            FROM
+                muon_rates
+            NATURAL JOIN
+                runs
+            WHERE
+                Label = ?
+            GROUP BY
+                Hall,
+                DetNo
+            ORDER BY
+                Hall,
+                DetNo
+            ''',
+            (label,)
+        )
+        livetimes_s = np.array(cursor.fetchall()).reshape(-1)
+    return livetimes_s
+
 def muon_efficiency(database, label):
     """Return an array of muon efficiencies from EH1-AD1 to EH3-AD4."""
     with common.get_db(database) as conn:
@@ -58,6 +84,37 @@ def muon_efficiency(database, label):
         )
         muon_effs = np.array(cursor.fetchall()).reshape(-1)
     return muon_effs
+
+def muon_total_counts(database, label):
+    """Return an array of muon counts from EH1-AD1 to EH3-AD4."""
+    with common.get_db(database) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT
+                SUM(`Count`)
+            FROM
+                muon_rates
+            NATURAL JOIN
+                runs
+            WHERE
+                Label = ?
+            GROUP BY
+                Hall,
+                DetNo
+            ORDER BY
+                Hall,
+                DetNo
+            ''',
+            (label,)
+        )
+        muon_counts = np.array(cursor.fetchall()).reshape(-1)
+    return muon_effs
+
+def muon_rate_Hz(database, label):
+    """Return an array of muon rates from EH1-AD1 to EH3-AD4."""
+    unvetoed_livetimes = unvetoed_livetime_s(database, label)
+    counts = muon_counts(database, label)
+    return counts / unvetoed_livetimes
 
 def multiplicity_efficiency(database, label):
     """Return an array of multiplicity efficiencies from EH1-AD1 to EH3-AD4."""
@@ -91,6 +148,39 @@ def multiplicity_efficiency(database, label):
         )
         mult_effs = np.array(cursor.fetchall()).reshape(-1)
     return mult_effs
+
+def singles_rate_Hz(database, label):
+    """Return an array of singles rates from EH1-AD1 to EH3-AD4."""
+    with common.get_db(database) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT
+                SUM(singles.Rate_Hz * Livetime_ns/Efficiency)/
+                SUM(Livetime_ns/Efficiency)
+            FROM
+                singles_rates AS singles
+            NATURAL JOIN
+                runs
+            INNER JOIN
+                muon_rates
+            USING (
+                RunNo,
+                DetNo,
+                Label
+            )
+            WHERE
+                Label = ?
+            GROUP BY
+                Hall,
+                DetNo
+            ORDER BY
+                Hall,
+                DetNo
+            ''',
+            (label,)
+        )
+        singles_rates = np.array(cursor.fetchall()).reshape(-1)
+    return singles_rates
 
 def coincidences_counts(database, label):
     """Return an array of coincidence counts from EH1-AD1 to EH3-AD4."""
